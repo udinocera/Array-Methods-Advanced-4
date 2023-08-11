e8494846f780e3263041f5c56103eccba9c64326
udinocera <udinocera@gmail.com>
2023-08-12 00:27:30 +0200

    esercizio

---
+++ script.js
@@ -0,0 +1,59 @@
+function uncompletedNotes(notes) {
+    notes.forEach(element => {
+        const notDone = element.todos.filter(todo => !todo.done)
+        notDone.forEach(el => console.log("Da fare: " + el.name))
+    });
+  }
+  
+  const notes = [
+    {
+      id: 1,
+      description: "Workout program",
+      todos: [
+        {
+          id: 1,
+          name: "Push ups - 10 x 3",
+          done: false,
+        },
+        {
+          id: 2,
+          name: "Abdominals - 20 x 3",
+          done: true,
+        },
+        {
+          id: 3,
+          name: "Tapis Roulant - 15min",
+          done: true,
+        },
+      ],
+    },
+    {
+      id: 2,
+      description: "Front-end Roadmap",
+      todos: [
+        {
+          id: 1,
+          name: "Learn HTML",
+          done: true,
+        },
+        {
+          id: 2,
+          name: "Learn CSS",
+          done: true,
+        },
+        {
+          id: 3,
+          name: "Learn JavaScript",
+          done: true,
+        },
+        {
+          id: 4,
+          name: "Learn Angular",
+          done: false,
+        },
+      ],
+    },
+  ];
+  
+  uncompletedNotes(notes);
+  
